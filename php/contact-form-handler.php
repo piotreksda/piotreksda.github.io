429ff685e25ebc365d8d303b4f286c186edc715c
piotreksda <piotreksda@gmail.com>
2020-05-15 15:53:38 +0200

    Jazda

--- php/contact-form-handler.php
+++ php/contact-form-handler.php
@@ -24,11 +24,13 @@ $email_subject = "Contact form submission: $name";
 $email_body = "You have received a new message. ".
 " Here are the details:\n Name: $name \n ".
 "Email: $email_address\n Message \n $message";
-$headers = "From: $myemail\n";
+$headers = "MIME-Version: 1.0";
+$headers .= "Content-type: text/html; charset=iso-8859-1";
+$headers .= "From: $myemail\n";
 $headers .= "Reply-To: $email_address";
 $headers .= "Content-type: text/html\r\n";
 mail($to,$email_subject,$email_body,$headers);
 //redirect to the 'thank you' page
-//header('Location: http://www.piotrusjestem.me');
+header('Location: /');
 }
 ?>
