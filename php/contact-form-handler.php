eb9711d4c0dbe9366f1c84c93be34df72cc785eb
piotreksda <piotreksda@gmail.com>
2020-05-15 16:02:21 +0200

    Jazda

--- php/contact-form-handler.php
+++ php/contact-form-handler.php
@@ -1,5 +1,5 @@
 <?php
-/*
+
 $errors = '';
 $myemail = 'piotrek.bartczak3@gmail.com';//<-----Put Your email address here.
 if(empty($_POST['name'])  ||
@@ -32,5 +32,5 @@ $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 //redirect to the 'thank you' page
 header('Location: /');
-}*/
+
 ?>
